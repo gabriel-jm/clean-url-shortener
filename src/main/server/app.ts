b0ae3cc4f70da2f1312976ab0f838ade143b9741
Gabriel Jose <gabriel.josemelo@hotmail.com>
2021-12-01 13:19:59 -0300

    feat: continue body data parsing

--- src/main/server/app.ts
+++ src/main/server/app.ts
@@ -1,28 +1,29 @@
-import { readLines } from 'io'
-
-export async function handleRequest(request: Request) {
-  const { pathname } = new URL(request.url)
-
+async function parseIncomingBody(request: Request) {
+  const { body } = request
+  
+  if (!body) return null
+  
   const dataArray: Uint8Array[] = []
 
   const writeStream = new WritableStream({
     write(chunk) {
       return new Promise((resolve) => {
-        console.log(chunk)
         dataArray.push(chunk)
         resolve()
       })
     }
   })
 
-  const { body } = request
+  await body.pipeTo(writeStream)
+  return dataArray.reduce((acc, value) => {
+    return acc + new TextDecoder('utf-8').decode(value)
+  }, '')
+}
 
-  if(body) {
-    await body.pipeTo(writeStream)
-    console.log(dataArray.reduce((acc, value) => {
-      return acc + new TextDecoder('utf-8').decode(value)
-    }, ''))
-  }
+export async function handleRequest(request: Request) {
+  const { pathname } = new URL(request.url)
+
+  const body = await parseIncomingBody(request)
 
   return new Response('hello', {
     headers: {
