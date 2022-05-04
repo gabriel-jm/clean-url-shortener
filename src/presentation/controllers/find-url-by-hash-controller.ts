2e91a5a01d0abd943734f3ddeb7b8251f9aad45c
Gabriel Jose <gabriel.josemelo@hotmail.com>
2022-05-04 15:28:06 -0300

    feat: create find url by hash controller

--- src/presentation/controllers/find-url-by-hash-controller.ts
+++ src/presentation/controllers/find-url-by-hash-controller.ts
@@ -1,7 +1,17 @@
+import { FindUrlByHash } from '@/domain/usecases/index.ts'
 import { Controller, HTTPResponse } from '@/presentation/protocols/index.ts'
+import { ok } from '@/presentation/helpers/index.ts'
+
+interface FindUrlByHashControllerParams {
+  hash: string
+}
 
 export class FindUrlByHashController implements Controller {
-  handle(params: unknown): Promise<HTTPResponse> {
-    throw new Error("Method not implemented.");
+  constructor(private readonly findUrlByHash: FindUrlByHash) {}
+
+  async handle(params: FindUrlByHashControllerParams): Promise<HTTPResponse> {
+    const url = await this.findUrlByHash.find(params.hash, new Date())
+
+    return ok({ url })
   }
 }
